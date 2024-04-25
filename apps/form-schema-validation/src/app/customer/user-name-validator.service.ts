0ec2ee0acb134e014f96d8a5bd4fe98901204599
Gregor Woiwode <gregor.woiwode@co-IT.eu>
2024-04-25 21:41:32 +0200

    perf: memoise async validation function

--- apps/form-schema-validation/src/app/customer/user-name-validator.service.ts
+++ apps/form-schema-validation/src/app/customer/user-name-validator.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Memoize } from 'fast-typescript-memoize';
 
 @Injectable({ providedIn: 'root' })
 export class UserNameValidator {
-  async validate(some: any) {
-    console.log(some);
+  // invalidates cache if
+  // - parameter "candidate" changes
+  // - promise rejects
+  @Memoize()
+  async canUserNameBeTaken(candidate: string): Promise<boolean> {
+    console.log('Check user name availability for', candidate);
 
     return Math.random() < 0.5;
   }
